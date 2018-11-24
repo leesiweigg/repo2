6f8fdd4e40b8390525632d7950c9e76e671e4845
lee <120955040@qq.com>
2018-11-24 20:53:34 +0800

    增加了一个方法

--- idea_git_test/src/com/heima/test/Demo.java
+++ idea_git_test/src/com/heima/test/Demo.java
@@ -6,4 +6,9 @@ public class Demo {
         System.out.println("被git管理");
         System.out.println("做一波修改");
     }
+
+    public void test(){
+        int a = 5;
+        System.out.println("a = " + a);
+    }
 }
