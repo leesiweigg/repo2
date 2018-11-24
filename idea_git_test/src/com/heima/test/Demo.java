67ba204a3683fdc70a003f4c24460f2c10daf8e9
lee <120955040@qq.com>
2018-11-24 20:57:56 +0800

    另一个人做出修改

--- idea_git_test/src/com/heima/test/Demo.java
+++ idea_git_test/src/com/heima/test/Demo.java
@@ -5,6 +5,8 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("被git管理");
         System.out.println("做一波修改");
+        //我是另一个人
+        System.out.println("helloword");
     }
 
     public void test(){
